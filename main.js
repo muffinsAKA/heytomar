958d106d58c39eef2c3b650fef756d00cad5bbc6
muffinsAKA <muffins@gmail.com>
2023-07-29 02:30:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const text = document.getElementById('tomar-text');
 
 async function getHypothetical() {
   try {
-    const response = await fetch('https://xvl6g9g9xk.execute-api.us-east-1.amazonaws.com/dev', {
+    const response = await fetch('https://xvl6g9g9xk.execute-api.us-east-1.amazonaws.com/dev/hypothetical', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
